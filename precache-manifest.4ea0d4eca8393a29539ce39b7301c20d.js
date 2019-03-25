98d86bd6e171d8df65199b8f0a9c7ebfb56f4719
unknown <lauri.rannu@gmail.com>
2019-03-25 15:12:36 +0200

    Updates

---
+++ precache-manifest.4ea0d4eca8393a29539ce39b7301c20d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9094eac82de99c698c17426b56dce42",
+    "url": "/portfolio/static/media/line.b9094eac.jpeg"
+  },
+  {
+    "revision": "48900a8a3cfff0bab8ba",
+    "url": "/portfolio/static/css/main.4f66d70d.chunk.css"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "440d9faaeef3aab451b7",
+    "url": "/portfolio/static/js/2.5b0bc8b6.chunk.js"
+  },
+  {
+    "revision": "811476e8bdca092125937696c3fa5a0e",
+    "url": "/portfolio/static/media/flowers.811476e8.jpg"
+  },
+  {
+    "revision": "48900a8a3cfff0bab8ba",
+    "url": "/portfolio/static/js/main.88fd6a22.chunk.js"
+  },
+  {
+    "revision": "024f44356e4e675b94897aba215c6d00",
+    "url": "/portfolio/static/media/city.024f4435.jpg"
+  },
+  {
+    "revision": "a45e5c4aef8c5c25c5c136dbe1da9ddc",
+    "url": "/portfolio/static/media/roses.a45e5c4a.jpg"
+  },
+  {
+    "revision": "a15981ca074d23073f01fac3b66b87d3",
+    "url": "/portfolio/static/media/street.a15981ca.jpg"
+  },
+  {
+    "revision": "fe7d08ef57f319cf8225fa51ab6b32cd",
+    "url": "/portfolio/static/media/wave.fe7d08ef.jpeg"
+  },
+  {
+    "revision": "5e83c3e35632f1f55addcc0efa33a5b2",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.824d439703a70f5686db49baa92df816.js"
+  "/portfolio/precache-manifest.4ea0d4eca8393a29539ce39b7301c20d.js"
 );
 
 workbox.clientsClaim();
